8eba1cdd42b57677bebcc7fafd2ada9f4fe2e644
maimai77 <kohta0707@gmail.com>
2018-03-01 17:57:29 +0900

    p68

--- public/app.js
+++ public/app.js
@@ -42,6 +42,10 @@ learnjs.buildCorrectFlash = function(problemNum) {
   return correctFlash;
 };
 
+learnjs.landingView = function() {
+  return learnjs.template('landing-view');
+};
+
 learnjs.problemView = function(data) {
   var problemNumber = parseInt(data, 10);
   var view = learnjs.template('problem-view');
@@ -73,6 +77,7 @@ learnjs.problemView = function(data) {
 learnjs.showView = function(hash) {
   var routes = {
     '#problem': learnjs.problemView,
+    '': learnjs.landingView,
   };
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
